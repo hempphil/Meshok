bb8cb4856d4cc895176a2583d714c3fee17fa19f
hempphil <thereisnobody@gmail.com>
2024-07-02 18:04:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const stopButton = document.getElementById('stop');
 const shovelButton = document.getElementById('shovel');
 const resetButton = document.getElementById('reset');
 
-
 const workPhrases = [
     "Вот это мешок! Ты заслужил перекур!",
     "Я в юности такие мешки десятками ворочал",
